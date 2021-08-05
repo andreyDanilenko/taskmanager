7778595b77d3d55638a6dc218c4a4515bf87004c
Andrey <danilenko.a.g@mail.ru>
2021-08-05 13:21:42 +0300

    Добовляем фильтрацию

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import { createLoadMoreButtonTemplate } from './view/load-more-button.js';
 import { createBoardTemplate } from './view/board.js';
 import { createTaskEditTemplate } from './view/task-edit.js';
 import { generateTask } from './mocks/task.js';
+import { generateFilter, taskToFilterMap } from './utils.js/filter.js';
 
 const TASK_COUNT = 4;
 //new Array() cоздает массив без элементов(только с длинной)
 //fill() создает элементы массива (по умолчанию undefined)
 //map() создает элемнт массива на каждый новый вызов функции()
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const filters = generateFilter(tasks);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -20,7 +22,7 @@ const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 render(siteHeaderElement, createSiteMenuTemplate(), 'beforeend');
-render(siteMainElement, createFilterTemplate(), 'beforeend');
+render(siteMainElement, createFilterTemplate(filters), 'beforeend');
 render(siteMainElement, createBoardTemplate(), 'beforeend');
 
 const boardElement = siteMainElement.querySelector('.board');
---
+++ src/utils.js/filter.js
@@ -0,0 +1,30 @@
+import { isTaskExpired, isTaskRepeating, isTaskExpiringToday } from './util';
+
+export const taskToFilterMap = {
+  all: (tasks) => tasks.filter((task) => !task.isArchive).length,
+  overdue: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => isTaskExpired(task.dueDate)).length,
+  today: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => isTaskExpiringToday(task.dueDate)).length,
+  favorites: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => task.isFavorite).length,
+  repeating: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => isTaskRepeating(task.repeating)).length,
+  archive: (tasks) => tasks.filter((task) => task.isArchive).length,
+};
+
+// Функция считающая длинну соттветсвующего флага 
+// Берем обьек значения которого это функция высчитывающая длину соответсвующего состояния задачи (флаг)
+// С помощью Object.entries(taskToFilterMap) создаем массив массивов (если ключей и значений больше 1) [[all, f(tasks)], ...] 
+// Методом map() создаем массив из обьектов где старые ключь и значение [all, f(tasks)] становятся значениями зарание заданных ключей
+// {name: all , count: f(tasks)}, функция f(tasks) принимает массив с данными вычисляя длину соответсвующего флага
+export const generateFilter = (tasks) => Object.entries(taskToFilterMap).map(
+  ([filterName, countTasks]) => ({
+    name: filterName,
+    count: countTasks(tasks),
+  }),
+);
