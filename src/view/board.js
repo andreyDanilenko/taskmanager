73dd93a5cebc3cbd5a2989b364f330353ad90019
Andrey <danilenko.a.g@mail.ru>
2021-08-05 23:23:44 +0300

    Разбиваем контейнер сортировки и сортировку

--- src/view/board.js
+++ src/view/board.js
@@ -1,10 +1,25 @@
-export const createBoardTemplate = () => (
-  `<section class="board container">
-    <div class="board__sort-list">
-      <a href="#" class="board__sort-item">SORT BY DEFAULT</a>
-      <a href="#" class="board__sort-item">SORT BY DATE up</a>
-      <a href="#" class="board__sort-item">SORT BY DATE down</a>
-    </div>
-    <div class="board__tasks"></div>
-  </section>`
-);
+import { createElement } from "../utils/util";
+
+const createBoardTemplate = () => '<section class="board container"></section>';
+
+export default class Board {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createBoardTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
