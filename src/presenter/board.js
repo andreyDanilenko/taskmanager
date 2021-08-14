4e734eebef084151424e85e2080e9c96a2e0f96a
Andrey <danilenko.a.g@mail.ru>
2021-08-14 16:59:38 +0300

    Добавляем перезнете для задачи

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -2,21 +2,24 @@ import BoardView from "../view/board";
 import SortView from '../view/sort';
 import TaskListView from "../view/task-list";
 import NoTaskView from "../view/task-no";
-import TaskCardView from "../view/task-card";
-import TaskEditView from "../view/task-edit";
+import TaskPresenter from './task'
 import LoadMoreButtonView from "../view/load-more-button";
-import { render, RenderPosition, replace, remove } from "../utils/render";
+import { render, RenderPosition, remove } from "../utils/render";
 
 const TASK_COUNT_PER_STEP = 8;
 
 export default class Board {
   constructor(boardContainer) {
     this._boardContainer = boardContainer;
-
+    this._renderedTaskCount = TASK_COUNT_PER_STEP;
     this._boardComponent = new BoardView();
     this._sortComponent = new SortView();
     this._taskListComponent = new TaskListView();
     this._noTaskComponet = new NoTaskView();
+
+    this._loadMoreButtonComponent = new LoadMoreButtonView();
+    this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this)
+
   }
   // Метод который вызывается извне, предает в себя данные, отрисовывая отх в том порядке в котором требуют методы презентера
   init(boardTasks) {
@@ -34,37 +37,8 @@ export default class Board {
   }
   // Метод для отрисовки одной задачи
   _renderTask(task) {
-    const taskCardComponent = new TaskCardView(task);
-    const taskEditComponent = new TaskEditView(task);
-    //Функция меняющая карточку на форму
-    const replaceCardToForm = () => {
-      replace(taskEditComponent, taskCardComponent)
-    };
-    // Функция меняющая форму на карточку
-    const replaceFormToCard = () => {
-      replace(taskCardComponent, taskEditComponent)
-    };
-    // При открытой форме при нажатии на клавишу ESC вызывает функцию замены формы на карточку и удаляет данный оброботчик
-    const onEscKeyDown = (evt) => {
-      if (evt.key === 'Escape' || evt.key === 'Esc') {
-        evt.preventDefault();
-        replaceFormToCard();
-        document.removeEventListener('keydown', onEscKeyDown);
-      }
-    };
-    // Событие по нажатию на кнопку EDit в закрытой карточке задачи 
-    // Передаем в метод классса компонента setEditClickHundler функцию которая будет присваиватьс\я значению callback
-    taskCardComponent.setEditClickHandler(() => {
-      replaceCardToForm();
-      document.addEventListener('keydown', onEscKeyDown);
-    });
-    // при нажатии кнопуи типа submit форма меняется на закрытую карточку и удаляет обработчик клавиши ESC
-    taskEditComponent.setFormSubmitHandler(() => {
-      replaceFormToCard();
-      document.removeEventListener('keydown', onEscKeyDown);
-    });
-
-    render(this._taskListComponent, taskCardComponent, RenderPosition.BEFOREEND)
+    const taskPresentor = new TaskPresenter(this._taskListComponent);
+    taskPresentor.init(task)
   }
   // Метод для отрисовки заданного количества задач
   _renderTasks(from, to) {
@@ -77,22 +51,18 @@ export default class Board {
     render(this._boardComponent, this._noTaskComponet, RenderPosition.BEFOREEND)
   }
   // Метод отрисовки кнопки 
+  _handleLoadMoreButtonClick() {
+    this._renderTasks(this._renderedTaskCount, this._renderedTaskCount + TASK_COUNT_PER_STEP);
+    this._renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (this._renderedTaskCount >= this._boardTasks.length) {
+      remove(this._loadMoreButtonComponent);
+    }
+  }
+
   _renderLoadMoreButton() {
-    let renderedTaskCount = TASK_COUNT_PER_STEP;
-    const loadMoreButtonComponent = new LoadMoreButtonView();
-    render(this._boardComponent, loadMoreButtonComponent, RenderPosition.BEFOREEND)
-
-    loadMoreButtonComponent.setClickHandler(() => {
-      this._boardTasks
-        .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-        .forEach((boardTask) => this._renderTask(boardTask));
-
-      renderedTaskCount += TASK_COUNT_PER_STEP;
-
-      if (renderedTaskCount >= this._boardTasks.length) {
-        remove(loadMoreButtonComponent);
-      }
-    });
+    render(this._boardComponent, this._loadMoreButtonComponent, RenderPosition.BEFOREEND);
+    this._loadMoreButtonComponent.setClickHandler(this._handleLoadMoreButtonClick);
   }
 
   _renderTasksList() {
