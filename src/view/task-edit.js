fe81d88ea56cc4fe3df3e0a71153f90beea8a1b8
Andrey <danilenko.a.g@mail.ru>
2021-08-27 14:52:11 +0300

    Добавляем обработчики на изменения состояния

--- src/view/task-edit.js
+++ src/view/task-edit.js
@@ -133,16 +133,77 @@ export default class TaskEdit extends AbstractView {
     // Превращаем полученную информацию с сервера в состояние для того чтобы его редактировать
     this._data = TaskEdit.parseTaskToData(task);  // Задача к данным
 
-    this._formSubmitHandler = this._formSubmitHandler.bind(this)
+    this._formSubmitHandler = this._formSubmitHandler.bind(this);
+    this._descriptionTextareaHandler = this._descriptionTextareaHandler.bind(this);
+    this._dueDateToggleHandler = this._dueDateToggleHandler.bind(this);
+    this._repeatingToggleHandler = this._repeatingToggleHandler.bind(this);
+
+    this._setInnerHandlers()
   }
 
   getTemplate() {
     // Отрисует состояние парсенное из актуальной информации с сервера
     return createTaskEditTemplate(this._data);
-
   }
-  // метод обновления шаблона
-  updateElement() {
+
+  restoreHandlers() {
+    this._setInnerHandlers();
+    this.setFormSubmitHandler(this._callback.formSubmit);
+  }
+  // Заносим обработчики кнопок по дате в отдельный метод
+  _setInnerHandlers() {
+    this.getElement()
+      .querySelector('.card__date-deadline-toggle')
+      .addEventListener('click', this._dueDateToggleHandler);
+    this.getElement()
+      .querySelector('.card__repeat-toggle')
+      .addEventListener('click', this._repeatingToggleHandler);
+    this.getElement()
+      .querySelector('.card__text')
+      .addEventListener('input', this._descriptionTextareaHandler);
+  }
+  // Метод оброботчика меняющий сотояние даты есть или нет
+  _dueDateToggleHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      isDueDate: !this._data.isDueDate,
+    })
+  }
+  // Метод оброботчика менющего состояние повторения даты есть или нет
+  _repeatingToggleHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      isRepeating: !this._data.isRepeating,
+    })
+  }
+  // Такой же метод как и все меняет состояние поля ввода и фиксирует его в будущем
+  _descriptionTextareaHandler(evt) {
+    evt.preventDefault();
+    this.updateData({
+      description: evt.target.value,
+    }, true);
+  }
+  // Метод обновления состояния при взамодействии с клиентом
+  updateData(update, justDataUpdating) { // Обновление состояния
+    if (!update) {
+      return;
+    }
+
+    this._data = Object.assign(
+      {},
+      this._data,
+      update,
+    );
+    // если значение данного паремтра true то функция завершает работу и не перерисовывает окно лишний раз
+    // Без данного флага с тексарии будет пропадать фокус так как после ввода каждого символа будеть происходить обновление карточки
+    if (justDataUpdating) {
+      return;
+    }
+
+    this.updateElement();
+  }
+  // метод обновления шаблона элемента
+  updateElement() { // Обновление элемента
     // Записываем создание нового элемента в переменную 
     const prevElement = this.getElement();
     // Возвращает родителя созданному элементу
@@ -157,6 +218,8 @@ export default class TaskEdit extends AbstractView {
     const newElement = this.getElement();
     // меням предыдущий элемент на обновленный newElement
     parent.replaceChild(newElement, prevElement);
+
+    this.restoreHandlers();
   }
 
   _formSubmitHandler(evt) {
